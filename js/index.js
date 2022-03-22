27381074b4ec38e24ec20155e481b4468b31102b
khomasl <gymnasium.yv@gmail.com>
2022-03-22 10:55:15 +0200

    feature/modif

--- js/index.js
+++ js/index.js
@@ -1,252 +1,97 @@
-const matchItems = [
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  //8
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  //15
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  //22
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 1,
-  },
-  {
-    left: 1,
-    top: 1,
-  },
-  {
-    left: 1,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  //29
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 1,
-  },
-  {
-    left: 1,
-    top: 1,
-  },
-  {
-    left: 1,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  //36
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  //43
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-  {
-    left: 0,
-    top: 0,
-  },
-]
-
+let numberMatches = 0
 let tabInd = -1
+let matchesMarkup = ''
+
+const isFlag = (el) =>
+  el.classList.contains('box-33') ||
+  el.classList.contains('box-35') ||
+  el.classList.contains('box-47')
+
+const isDefaultMatches = (el) =>
+  el.classList.contains('box-48') ||
+  el.classList.contains('box-49') ||
+  el.classList.contains('box-50') ||
+  el.classList.contains('box-51') ||
+  el.classList.contains('box-62') ||
+  el.classList.contains('box-63') ||
+  el.classList.contains('box-64') ||
+  el.classList.contains('box-65')
+
 const nodeSpan = '<span class="handle-move"></span>'
 
-const blockMatches = document.querySelector('.block-matches')
-
-const makeMatchItemsMarkup = ({ left, top }) => {
-  tabInd = tabInd + 2
-  const spanLeft = left === 1 ? nodeSpan : ''
-  const spanTop = top === 1 ? nodeSpan : ''
-
-  return `
+const cellMarkup = (idx, spanLeft, spanTop) => `
   <div class="cell-matches">
-        <a class="bx bx-1 box-${tabInd}" data-yes="${left}">${spanLeft}</a>
-        <a class="bx bx-2 box-${
-          tabInd + 1
-        }" data-yes="${top}">${spanTop}</a>    
+        <a class="bx bx-1 box-${idx}">${spanLeft}</a>
+        <a class="bx bx-2 box-${idx + 1}">${spanTop}</a>    
   </div>`
+
+const makeMatchesMarkup = () => {
+  for (let i = 1; i < 50; i++) {
+    tabInd += 2
+    switch (i) {
+      case 24:
+      case 31:
+        matchesMarkup += cellMarkup(tabInd, '', nodeSpan)
+        break
+
+      case 25:
+      case 32:
+        matchesMarkup += cellMarkup(tabInd, nodeSpan, nodeSpan)
+        break
+
+      case 26:
+      case 33:
+        matchesMarkup += cellMarkup(tabInd, nodeSpan, '')
+        break
+
+      default:
+        matchesMarkup += cellMarkup(tabInd, '', '')
+        break
+    }
+  }
 }
 
-const strWithMatchItemsMarkup = matchItems.map(makeMatchItemsMarkup).join('')
-blockMatches.insertAdjacentHTML('afterbegin', strWithMatchItemsMarkup)
+makeMatchesMarkup()
 
-document.querySelector('.box-33').setAttribute('data-yes', '1')
-document.querySelector('.box-35').setAttribute('data-yes', '1')
-document.querySelector('.box-47').setAttribute('data-yes', '1')
+const blockMatches = document.querySelector('.block-matches')
+blockMatches.insertAdjacentHTML('afterbegin', matchesMarkup)
+
+const countMatches = document.querySelector('.count-matches')
 
 blockMatches.addEventListener('click', toggleMatch)
 
 function toggleMatch(event) {
-  if (event.target.nodeName != 'A' && event.target.nodeName != 'SPAN') return
+  event.preventDefault()
 
-  if (event.target.nodeName === 'SPAN') {
-    event.target.remove()
+  const el = event.target
+  if (el.nodeName != 'A' && el.nodeName != 'SPAN') return
+
+  if (numberMatches > 3) {
+    alert('Вы исчерпали попытки. Обновите страницу и попробуйте заново')
     return
   }
 
-  const countSpans = document.querySelectorAll('span').length
+  if (el.nodeName === 'SPAN') {
+    el.remove()
+    numberMatches += 1
+    countMatches.textContent = `${numberMatches}`
+    return
+  }
+
+  const countSpans = document.querySelectorAll('.handle-move').length
   if (countSpans > 7) return
 
-  if (event.target.nodeName === 'A') {
-    const valueYes = event.target.dataset.yes
+  if (el.nodeName === 'A') {
+    el.insertAdjacentHTML('afterbegin', nodeSpan)
 
-    if (valueYes === '1') {
-      event.target.insertAdjacentHTML('afterbegin', nodeSpan)
+    if (isFlag(el)) el.classList.toggle('flag')
+
+    if (isDefaultMatches(el)) {
+      numberMatches -= 1
+      countMatches.textContent = `${numberMatches}`
     }
   }
+
+  if (document.querySelectorAll('.flag').length === 3)
+    alert('Поздравляю! Вы выиграли')
 }
